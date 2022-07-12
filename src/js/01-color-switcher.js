0490e07b35261d07c688ca307ec6c119e435c8e8
mark savka <mark.savka.it@gmail.com>
2022-07-12 11:33:00 +0300

    copleted first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,34 @@
-const buttonStart = document.querySelector("[data-start]");
-const buttonStop = document.querySelector("[data-stop]");
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+let timer;
 
+buttonStart.addEventListener('click', e => {
+  e.preventDefault();
+  changeBodyColor();
+  timer = setInterval(changeBodyColor, 1000);
+  disabledButtonReverse();
+});
+
+buttonStop.addEventListener('click', e => {
+  e.preventDefault();
+  clearInterval(timer);
+  stopBodyColor();
+  disabledButtonReverse();
+});
+
+function randomColor() {
+  return '#'.concat(Math.floor(Math.random() * 16777215).toString(16));
+}
+
+function stopBodyColor() {
+  document.body.style.backgroundColor = '#fff';
+}
+function changeBodyColor() {
+  console.log('randomColor', randomColor());
+  document.body.style.backgroundColor = randomColor();
+}
+function disabledButtonReverse() {
+  buttonStart.disabled = !buttonStart.disabled;
+  buttonStop.disabled = !buttonStop.disabled;
+}
