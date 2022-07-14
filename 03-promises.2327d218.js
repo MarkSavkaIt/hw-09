a63b87d2af8c3b0a7d9c20121492dd123e340b85
MarkSavkaIt <90281252+MarkSavkaIt@users.noreply.github.com>
2022-07-14 08:39:29 +0000

    Deploying to gh-pages from @ MarkSavkaIt/hw-09@53c3f839f1b7f9bbabbf4d61077abb11c5b9e8e8 🚀

--- 03-promises.2327d218.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form");const e=document.querySelector('input[name="delay"]'),o=document.querySelector('input[name="step"]'),t=document.querySelector('input[name="amount"]');console.log(e,o,t);
-//# sourceMappingURL=03-promises.2327d218.js.map
